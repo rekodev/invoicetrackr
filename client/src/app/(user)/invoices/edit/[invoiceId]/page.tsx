ee1fb13ca438013edbac3948e7e541cbf3f8a474
rekodev <reko.jsx@gmail.com>
2025-03-28 21:07:30 +0200

    Update edit invoice page

--- client/src/app/(user)/invoices/edit/[invoiceId]/page.tsx
+++ client/src/app/(user)/invoices/edit/[invoiceId]/page.tsx
@@ -1,6 +1,6 @@
-import { getInvoice } from '@/api';
-import { auth } from '@/auth';
-import InvoiceForm from '@/components/invoice/InvoiceForm';
+import { getInvoice, getUser } from "@/api";
+import { auth } from "@/auth";
+import InvoiceForm from "@/components/invoice/InvoiceForm";
 
 type Params = Promise<{ invoiceId: string }>;
 
@@ -11,12 +11,13 @@ const EditInvoicePage = async ({ params }: { params: Params }) => {
   if (!session?.user?.id) return null;
 
   const response = await getInvoice(Number(session.user.id), Number(invoiceId));
+  const { data: user } = await getUser(Number(session.user.id));
 
   return (
     <section>
       <InvoiceForm
+        user={user}
         currency={session.user.currency}
-        userId={Number(session.user.id)}
         invoiceData={response.data.invoice}
       />
     </section>
