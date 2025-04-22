e78fe8279bda1a6a0a4d2ed720b418229e545d69
rekodev <reko.jsx@gmail.com>
2025-04-22 09:25:50 +0300

    Add host, port and secure options to nodemailer transporter

--- server/src/config/nodemailer.ts
+++ server/src/config/nodemailer.ts
@@ -3,6 +3,9 @@ import "dotenv/config";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: "invoicetrackr@gmail.com",
     pass: process.env.EMAIL_PASSWORD,
