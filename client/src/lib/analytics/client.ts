2200acd9f36374dff0b29b74a7597c9a99580b57
rekodev <reko.jsx@gmail.com>
2026-06-27 18:11:20 +0300

    fix(REK-53): capture initialized analytics events

--- client/src/lib/analytics/client.ts
+++ client/src/lib/analytics/client.ts
@@ -10,12 +10,14 @@ export const initializeAnalytics = () => {
   const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
   const host = process.env.NEXT_PUBLIC_POSTHOG_HOST;
 
-  if (!key || isInitialized) return false;
+  if (!key) return false;
+  if (isInitialized) return true;
 
   posthog.init(key, {
     api_host: host || 'https://eu.i.posthog.com',
     autocapture: false,
     capture_pageview: false,
+    capture_performance: false,
     disable_session_recording: true,
     persistence: 'localStorage+cookie',
     opt_out_capturing_by_default: true
